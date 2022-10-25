858e2abbe7d4d1bd9a62b7ae08db95ae833762db
kjh <a4141407@gmail.com>
2022-10-25 22:19:56 +0900

    게시글 전체 조회 화면 구현

--- src/main/java/com/spring/book/service/posts/PostsService.java
+++ src/main/java/com/spring/book/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.spring.book.service.posts;
 
 import com.spring.book.domain.posts.Posts;
 import com.spring.book.domain.posts.PostsRepository;
+import com.spring.book.web.dto.PostsListResponseDto;
 import com.spring.book.web.dto.PostsResponseDto;
 import com.spring.book.web.dto.PostsSaveRequestDto;
 import com.spring.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +33,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
