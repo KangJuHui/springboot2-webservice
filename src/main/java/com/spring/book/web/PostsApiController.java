3cbd3c784a7199b77bb7b62ed87be96f940a1e6a
kjh <a4141407@gmail.com>
2022-11-12 15:11:10 +0900

    게시글 삭제 화면 구현

--- src/main/java/com/spring/book/web/PostsApiController.java
+++ src/main/java/com/spring/book/web/PostsApiController.java
@@ -1,11 +1,15 @@
 package com.spring.book.web;
 
 import com.spring.book.service.posts.PostsService;
+import com.spring.book.web.dto.PostsListResponseDto;
 import com.spring.book.web.dto.PostsResponseDto;
 import com.spring.book.web.dto.PostsSaveRequestDto;
 import com.spring.book.web.dto.PostsUpdateRequestDto;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -27,5 +31,17 @@ public class PostsApiController {
     public PostsResponseDto findById(@PathVariable Long id)
     {
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        System.out.println("controller delete 함수 진입");
+        postsService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
     }
 }
