94f460d4ce3c0b23c3b8730a5600e55afc9763b2
kjh <a4141407@gmail.com>
2022-11-29 22:08:25 +0900

    OAuth 로그인 API 구현 - 네이버 로그인 API 추가

--- src/main/java/com/spring/book/config/auth/SecurityConfig.java
+++ src/main/java/com/spring/book/config/auth/SecurityConfig.java
@@ -19,21 +19,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
-                    //URL 별 관리를 설정하는 옵션
+                //URL 별 관리를 설정하는 옵션
                     .authorizeRequests()
                     .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest().authenticated()
                 .and()
-                    //logout 기능에 대한 여러 설정의 진입점
+                //logout 기능에 대한 여러 설정의 진입점
                     .logout()
                         .logoutSuccessUrl("/")
                 .and()
-                    //login 기능에 대한 여러 설정의 진입점
+                //login 기능에 대한 여러 설정의 진입점
                     .oauth2Login()
                         .userInfoEndpoint()
-                            .userService(customOAuth2UserService);
-                            //로그인 성공시 후속 조치를 진행할 UserService 인터페이스 구현체를 등록
+                        .userService(customOAuth2UserService);
+                        //로그인 성공시 후속 조치를 진행할 UserService 인터페이스 구현체를 등록
     }
-
 }
